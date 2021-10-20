381316110f7f87974a1ba6cd10007cec128b2f5a
Alvis <1099147061@qq.com>
2021-10-20 15:23:06 +0800

    登录功能初始化

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -24,6 +24,10 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',//函数名与小括号间需要空格  关闭
+    'no-mixed-spaces-and-tabs': 'off',//空格和tab不能混用  关闭
+    'no-tabs': 'off',//
+    'no-multi-spaces': 'off' //禁止连续多个空格  关闭
   }
 }
---
+++ src/components/user/Login.vue
@@ -0,0 +1,73 @@
+// eslint-disable-next-line
+<template>
+  <!-- 登录卡片 -->
+  <el-card class="box-card">
+    <!-- 欢迎语 -->
+    <div slot="header" class="clearfix">
+      <span>请登录</span>
+    </div>
+
+    <!-- 登录信息 -->
+    <div>
+      <!-- 内容（表格） -->
+      <el-form :model="loginForm" :rules="loginRules" ref="form">
+        <el-form-item  prop="username">
+          <el-input type="text" auto-complete="false" v-model="loginForm.username" placeholder="请输入用户名"/>
+        </el-form-item>
+
+        <el-form-item  prop="password">
+          <el-input type="password" auto-complete="false" v-model="loginForm.password" placeholder="请输入密码"/>
+        </el-form-item>
+
+        <el-form-item>
+          <el-checkbox v-model="checked">记住我</el-checkbox>
+        </el-form-item>
+
+        <el-form-item >
+          <el-button type="primary" style="width: 100%;" @click='submit'>登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+  	return {
+  		loginForm: {
+  			username: '',
+  			password: ''
+  		},
+  		checked: '',
+      loginRules: {
+  			username: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
+  			password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
+  		}
+  	}
+  }
+}
+</script>
+<style scoped="scoped">
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+
+.box-card {
+  width: 480px;
+}
+</style>
