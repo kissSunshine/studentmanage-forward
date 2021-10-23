a5a4a2a42d6a557646f622ca3e1e26a397ba5f0f
sunshine <1099147061@qq.com>
2021-10-23 17:52:37 +0800

    头部组件完成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,58 @@
 <template>
+  <header>
+
+    <div class="l-content">
+      <el-button icon="el-icon-menu" size="mini"></el-button>
+      <h3>首页</h3>
+    </div>
+
+    <div class="r-content">
+      <el-dropdown trigger="click" size="mini">
+        <span class="el-dropdown-link">
+          <img :src="userImg" class="user"/>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+
+  </header>
 </template>
 
 <script>
 export default {
-  name: 'Header'
+  name: 'Header',
+  data () {
+    return {
+      userImg: require('../assets/img/profile/Winnie.jpg')
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped="scoped">
+header {
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.l-content {
+  display: flex;
+  align-items: center;
+}
+
+.l-content > .el-button {
+  margin-right: 20px;
+}
+
+.el-dropdown-link > .user {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  padding-top: 4px;
+}
 </style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -2,9 +2,9 @@
   <el-container style="height: 100%;">
     <!-- 左侧导航 -->
     <el-aside width="200px">
-      <el-menu :unique-opened="true">
+      <el-menu :unique-opened="true" width="auto" style="height: 100%;" background-color="#acacac">
         <h3 style="margin-left: 30px;margin-right: 40px;">大熊数学</h3>
-        <MenuList style="height: 100%;" :menuList="this.menuList"></MenuList>
+        <MenuList :menuList="this.menuList"></MenuList>
       </el-menu>
     </el-aside>
     <!-- 右侧内容 -->
@@ -139,5 +139,8 @@ export default {
 
 </script>
 
-<style>
+<style scoped="scoped">
+.el-header {
+  background: #acacac;
+}
 </style>
